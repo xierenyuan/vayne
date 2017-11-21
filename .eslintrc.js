d4b1b4d98bec0bae1ded544788cbdb9a2e7910c7
谢仁洪 <xierenhong@renrenche.com>
2017-11-21 11:00:33 +0800

    处理webpack 之前备份

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -7,23 +9,19 @@ module.exports = {
   env: {
     browser: true,
   },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
-  'globals': {
-    'describe': false
-  },
+  // add your custom rules here
   'rules': {
+    // allow paren-less arrow functions
     'arrow-parens': 0,
+    // allow async-await
     'generator-star-spacing': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'semi': [2, 'never'],
-    'max-len': [2, 200, 2],
-    'generator-star-spacing': 0,
-    'space-before-function-paren': 0,
-    'padded-blocks': 0,
-    'no-var': 0
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
